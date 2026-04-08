e3050980726597086fed33ae4b9b1ae586c8fecb
Karol <karolgierlach@icloud.com>
2026-04-08 13:11:50 +0200

    github pages fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.IS_GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
     output: "export",
     images: {
-        unoptimized: true, 
+        unoptimized: true,
     },
+    basePath: isGithubPages ? "/7mov_SplashPage" : "",
+    assetPrefix: isGithubPages ? "/7mov_SplashPage" : "",
 };
 
 export default nextConfig;
